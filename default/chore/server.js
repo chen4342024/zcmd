05645741a5d3ab4bf1044fbbd45bbadee146f697
chen_jia_jia <chenjiajia@zuzuche.com>
2017-11-22 16:39:02 +0800

    本地开发

--- default/chore/server.js
+++ default/chore/server.js
@@ -1,8 +1,16 @@
 const superagent = require('superagent');
 const path = require('path');
 const fs = require('fs');
+const chalk = require('chalk');
+const cookie = require('cookie-parser');
+const httpProxy = require('express-http-proxy');
+const getCurrBranch = require('./getBranch');
+
 // 开发服务器
-const HOST = 'm_tidl.zuzuche.net';
+const branch = getCurrBranch();
+const HOST = `m_${branch}.zuzuche.net`;
+
+console.log(chalk.green("当前分支名 ----> " + HOST));
 
 // const HOST = 'http://172.16.70.30:8989/';
 const PROJECT_PATH = process.cwd();
@@ -13,100 +21,132 @@ fs.writeFile(LOG_FILE, '', {
     encoding: 'utf8'
 });
 
-module.exports = function setup(app, Server) {
-    app.get('/static/images/s-325472601571f31e1bf00674c368d335.gif', function (req, res) {
-        let fs = Server.middleware.fileSystem;
-        let filePath = path.join(PROJECT_PATH, 'dist', `images/s.gif`);
+module.exports = function setup(publicPath) {
 
-        let fileContent = '404 from webpack-dev-server';
-        if (fs.existsSync(filePath)) {
-            fileContent = fs.readFileSync(filePath);
-            res.writeHead(200, {'Content-Type': 'image/gif' });
-        }
-        res.end(fileContent);
-    });
+    return function (app, Server) {
+        app.use(cookie());
 
-    app.get('/', function (req, res) {
-        return proxy(req, res);
-    });
+        //处理图片问题
+        app.get(/\.(?:js|css|png|jpg|gif|jpeg)/, httpProxy(HOST, {
+            filter: function (req, res) {
+                return req.url.indexOf(publicPath) === -1;
+            }
+        }));
 
-    app.get(/\.(?:html|php)/, function (req, res) {
-        return proxy(req, res);
-    });
-
-    function proxy(req, res) {
-        let url = req.url;
-
-        fetchTemplateName(url).then(function (data) {
-            let templateName = data.route;
-            let templateString = readTemplateString(Server, templateName);
-
-            return renderTemplateByServer(url, templateString).then(function (
-                html) {
-                res.send(html);
-            });
-        }).catch(function (err) {
-            res.status(500);
-            res.send(
-                `<pre>status: ${err.status}\nmessage: ${err.message}\nstack: ${err.stack}</pre>`
-            );
+        app.get(/io_js\.php/, (req, res) => {
+            return httpProxy(HOST, {})
         });
+
+        //拦截url中带php,html的请求
+        app.get(/\.(?:html|php)/, function (req, res, next) {
+            if (req.xhr) {
+                //处理ajax请求
+                return reWriteProxy(req, res);
+            } else {
+                return proxy(req, res);
+            }
+        });
+
+        //拦截url中post类ajax请求
+        app.post(/\.(?:html|php)/, function (req, res, next) {
+            if (req.xhr) {
+                //处理ajax请求
+                return reWriteProxy(req, res);
+            }
+        });
+
+        //处理模板返回问题
+        function proxy(req, res) {
+            getTemplateName(req)
+                .then(function (templateName) {
+                    let templateString = readTemplateString(Server, templateName);
+
+                    return renderTemplateByServer(req, templateString).then(function (html) {
+                        res.send(html);
+                    });
+                })
+                .catch(function (err) {
+                    res.status(500);
+                    res.send(
+                        `
+                            <p>获取模板名的时候出错了</p>
+                            <pre>status: ${err.status}\nmessage: ${err.message}\nstack: ${err.stack}</pre>
+                        `
+                    );
+                });
+        }
+
+        //重定向请求
+        function reWriteProxy(req, res) {
+            let httpProxyFunc = httpProxy(HOST, {
+                filter: function (req, res) {
+                    return req.url.indexOf(publicPath) === -1;
+                }
+            });
+            return httpProxyFunc(req, res);
+        }
     }
 };
 
 // 从服务器获取模板名
 // url 域名后面的 path 和 query
-function fetchTemplateName(url) {
+function getTemplateName(req) {
+    let url = req.url;
     if (!fetchTemplateName._cache) {
         fetchTemplateName._cache = {};
     }
 
     if (fetchTemplateName._cache[url]) {
         return Promise.resolve(fetchTemplateName._cache[url]);
+    } else {
+        return fetchTemplateName(req)
+            .then((data) => {
+                let templateName = extractTemplateName(data);
+                fetchTemplateName._cache[url] = templateName;
+                return templateName;
+            });
     }
+}
 
+
+//发起获取模板名字的请求
+function fetchTemplateName(req) {
+    let url = req.url;
     let api = `${HOST}${url}`;
     let query = {
-        __is_dev_template:1,
-        p_file:1
+        __is_dev_template: 1,
+        p_file: 1
     };
-
     log(`request url:${api} query:${JSON.stringify(query)}`);
-
+    let cookies = cookieToString(req.cookies);
     return superagent
         .get(api)
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json')
-        .set('Cookie', '_cpan=chenjiajia')
+        .set('Cookie', cookies)
+        .set('User-Agent', req.get('user-agent'))
         .buffer(true)
-        .query(query).then(function (res) {
-            let body = res.body;
-
-            log(`response url:${api} `);
-            log(`res --> :${JSON.stringify(res)}`);
-            log(`res.status --> :${JSON.stringify(res.status)}`);
-            log(`res.text --> :${JSON.stringify(res.text)}`);
-            log(`res.body---> :${JSON.stringify(body)}`);
-
-            if (body.code) {
-                throw new Error(body.msg);
+        .query(query)
+        .then(function (res) {
+            log(`response url:${api} query:${res.text}`);
+            try {
+                let data = JSON.parse(res.text);
+                return data;
+            } catch (e) {
+                throw e;
             }
-
-            // cache
-            fetchTemplateName._cache[url] = body.data;
-            return body.data;
-        }).catch(function (err) {
+        }).catch((err) => {
             log(`response url:${api} status:${err.status} error:${err.message}`);
             throw err;
         });
 }
 
-
 // 请求服务端 渲染模板
 // url 域名后面的 path 和 query
 // templateString  前端 handlebars 渲染后生成的内容作为后端模板
-function renderTemplateByServer(url, templateString) {
-    url = `${HOST}/v2${url}`;
+function renderTemplateByServer(req, templateString) {
+
+    let url = `${HOST}${req.url}`;
 
     let requestBody = {};
 
@@ -116,25 +156,26 @@ function renderTemplateByServer(url, templateString) {
             '_debug_arr': 1
         };
     } else {
-        templateString = encodeURIComponent(templateString);
+        // templateString = encodeURIComponent(templateString);
         requestBody = {
-            'template_html': templateString
+            '__template_html': templateString,
+            '__is_dev_template': 1
         };
     }
 
     log(`request url:${url} template_html:${templateString.length}`);
-    // log(`\n\nrequest\n\n${templateString}\n\n`);
 
+    let cookies = cookieToString(req.cookies);
     return superagent
         .post(url)
         .set('content-type', 'application/x-www-form-urlencoded')
-        .set('Cookie', 'allow_test_ip=1')
+        .set('Cookie', cookies)
+        .set('User-Agent', req.get('user-agent'))
         .send(requestBody)
         .then(function (response) {
             // superagent 不需要解析 text/html 数据，且放在 .text 属性
             let body = response.text;
-            log(`response url:${url} body:${body.length}`);
-            // log(`\n\nresponse\n\n${body}\n\n`);
+            log(`response url:${url}  body:${body.length}`);
             return body;
         }).catch(function (err) {
             log(`response url:${url} status:${err.status} error:${err.message}`);
@@ -146,13 +187,13 @@ function renderTemplateByServer(url, templateString) {
 // 从webpack-dev-server读取模板内容
 function readTemplateString(Server, name) {
     let fs = Server.middleware.fileSystem;
-    let filePath = path.join(TEMPLATE_PATH, `${name}.html`);
+    let filePath = path.join(TEMPLATE_PATH, `${name}`);
 
-    let fileContent = '404 from webpack-dev-server';
+    let fileContent = `从webpack-dev-server找不到对应的模板内容，模板路径：${filePath}`;
     if (fs.existsSync(filePath)) {
         fileContent = fs.readFileSync(filePath, 'utf8');
     }
-
+    log(`readTemplateString ---> ${filePath} ; length ---> ${fileContent.length}`);
     return fileContent;
 }
 
@@ -165,4 +206,22 @@ function log(message) {
             console.log(`插入日志出错 ./chore/server.js log(message)`, err.message);
         }
     });
+}
+
+//从返回的file中提取模板名称
+function extractTemplateName(data) {
+    let file = data.file;
+    log(`extractTemplateName ---> ${file}`);
+    let paths = file.split('dist/html/');
+    return paths[1] || '';
+}
+
+// 将cookie序列化为字符串
+function cookieToString(cookies = {}) {
+    let cookieStr = "_cpan=limingchang;";
+    let keys = Object.keys(cookies);
+    keys.forEach((key) => {
+        cookieStr += `${key}=${cookies[key]};`
+    });
+    return cookieStr;
 }
